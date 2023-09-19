b5e170ef1a6f65fb2a137ef4c1c94c23821bc20b
xingye <1181954490@qq.com>
2023-09-19 22:29:57 +0800

    feat -l 对象拍平

--- 07-对象拍平.js
+++ 07-对象拍平.js
@@ -1,15 +1,44 @@
-const input = { a: { b: { c: { f: 123 } } }, d: { e: { f: 456 } } }
-let output = {}
-function fn(input, key) {
-  for (let item in input) {
-    if (typeof input[item] == 'object') {
-      fn(input[item], key + item)
-    } else {
-      output[key + item] = input[item]
+const obj = {
+  a: 1,
+  b: [1, 2, { c: true }, [3]],
+  c: { e: 2, f: 3 },
+  g: null
+}
+function flatten(myObj) {
+  const flatObj = {} // 用于存储扁平化后的对象
+  let flag = null // 标志，用于记录当前的键名
+
+  function formatKey(obj, keyName) {
+    for (let key in obj) {
+      if (typeof obj[key] === 'object' && obj[key] !== null) {
+        // 值为对象时，递归处理
+        if (!keyName) {
+          formatKey(obj[key], key) // 如果没有键名，直接使用当前的键名
+        } else {
+          if (Array.isArray(obj)) {
+            formatKey(obj[key], `${keyName}[${key}]`) // 处理属性值为数组的情况
+          } else {
+            formatKey(obj[key], `${keyName}.${key}`) // 处理属性值为对象的情况
+          }
+        }
+      } else {
+        // 值不为对象或者值为null时，将键值对添加到扁平化对象中
+        if (!keyName) {
+          flatObj[key] = obj[key] // 如果没有键名，直接使用当前的键名
+        } else {
+          if (Array.isArray(obj)) {
+            flatObj[`${keyName}[${key}]`] = obj[key] // 处理属性值为数组的情况
+          } else {
+            flatObj[`${keyName}.${key}`] = obj[key] // 处理属性值为对象的情况
+          }
+        }
+      }
     }
   }
+
+  formatKey(myObj, flag) // 初始调用，开始扁平化
+
+  return flatObj // 返回扁平化后的对象
 }
-for (let item in input) {
-  fn(input[item], item)
-}
-console.log(output)
+
+console.log(flatten(obj))
