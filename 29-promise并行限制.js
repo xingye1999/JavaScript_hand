e1cdde934c1b2c28540a41b1de1f0b69d7b7a316
xingye1999 <1181954490@qq.com>
2023-10-04 00:58:32 +0800

    feat -l 增加promise并行限制

--- 29-promise并行限制.js
+++ 29-promise并行限制.js
@@ -2,37 +2,34 @@ class Scheduler {
   constructor() {
     this.queue = []
     this.maxCount = 2
-    this.runCount = 0
+    this.runCounts = 0
   }
-
   add(promiseCreator) {
     this.queue.push(promiseCreator)
   }
-
   taskStart() {
     for (let i = 0; i < this.maxCount; i++) {
       this.request()
     }
   }
-
   request() {
-    if (!this.queue.length || this.runCount >= this.maxCount) {
+    if (!this.queue.length || this.runCounts >= this.maxCount) {
       return
     }
-    this.runCount++
+    this.runCounts++
     const task = this.queue.shift()
+    //这一步是为了调用完毕，继续并行调用
     task().then(() => {
-      this.runCount--
+      this.runCounts--
       this.request()
     })
   }
 }
 
-const timeout = (time) => {
-  return new Promise((resolve) => {
+const timeout = (time) =>
+  new Promise((resolve) => {
     setTimeout(resolve, time)
   })
-}
 
 const scheduler = new Scheduler()
 
@@ -44,9 +41,9 @@ const addTask = (time, order) => {
   )
 }
 
-addTask(10000, 1) // 10000ms后输出 任务1完成
-addTask(5000, 2) // 5000ms后输出 任务2完成
-addTask(3000, 3) // 8000ms后输出 任务3完成
-addTask(4000, 4) // 11000ms后输出 任务4完成
-addTask(5000, 5) // 15000ms后输出 任务5完成
+addTask(1000, "1")
+addTask(500, "2")
+addTask(300, "3")
+addTask(400, "4")
+
 scheduler.taskStart()
