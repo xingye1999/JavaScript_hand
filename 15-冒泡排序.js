d977fd780f269ac469bd983228627c59518a6bda
xingye1999 <1181954490@qq.com>
2023-10-04 14:58:13 +0800

    feat -l 快速排序

--- 15-冒泡排序.js
+++ 15-冒泡排序.js
@@ -1,11 +1,12 @@
 function bubbleSort(arr) {
-  for (let i = 1; i <= arr.length - 1; i++) {
-    for (let j = 0; j <= arr.length - 1 - i; j++) {
-      if (arr[j] < arr[j + 1]) [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
+  //趟数
+  for (let i = arr.length - 1; i >= 0; i--) {
+    for (let j = 0; j < i; j++) {
+      if (arr[j] > arr[j + 1]) [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
     }
   }
   return arr
 }
 
 let arr = [1, 5, 2, 8, 6, 4, 3, 9, 7]
-console.log('arr', bubbleSort(arr))
+console.log("arr", bubbleSort(arr))
