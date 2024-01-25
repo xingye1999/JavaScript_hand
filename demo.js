d20f9f6b4417e99d9a1991fe2054ac06a48a7483
xingye1999 <1181954490@qq.com>
2024-01-25 11:46:53 +0800

    feat -l 增加节流、防抖的html

--- demo.js
+++ demo.js
@@ -1,7 +1,35 @@
-console.log(null == null)
-console.log(null === null)
-console.log(undefined == undefined)
-console.log(undefined === undefined)
+// 防抖
 
-console.log(null == undefined)
-console.log(null === undefined)
+function debounce(fn, delay) {
+  var timer = null
+  return function () {
+    clearTimeout(timer)
+    timer = setTimeout(() => {
+      fn.apply(this, arguments)
+    }, delay)
+  }
+}
+
+// 节流
+function throttle(fn, time) {
+  var lastTime = 0
+  return function () {
+    var nowTime = Data.now()
+    if (nowTime - lastTime > time) {
+      fn.apply(this, arguments)
+      lastTime = nowTime
+    }
+  }
+}
+
+// 节流
+function throttle1(fn, time) {
+  var timer = null
+  return function () {
+    if (timer) return
+    timer = setTimeout(() => {
+      fn.apply(this, arguments)
+      timer = null
+    }, time)
+  }
+}
